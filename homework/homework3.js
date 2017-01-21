375b76bfaf04e9027467da92937e716d145dc78f
PavelGora <spec4web@gmail.com>
2017-01-21 19:21:19 +0300

    executeRequest function added

--- homework/homework3.js
+++ homework/homework3.js
@@ -1,4 +1,30 @@
-player.showHighScoreList = function(pageToken) {
+var executeRequest = function (isTestMode, response) {
+    //Here we can apply any test logic we need
+    console.log('High score', response);
+    if (response.error) {
+        alert('Error ' + response.error.code + ': ' + response.message);
+        return;
+    }
+    var root = document.getElementById('highScoreListDiv');
+    player.createPlayerList(root, response.items, true);
+    if (response.prevPageToken) {
+        root.appendChild(
+            utilities.createButton('Prev', response.prevPageToken,
+                function (event) {
+                    player.showHighScoreList(event.target.value, false);
+                }));
+    }
+    if (response.nextPageToken) {
+        root.appendChild(
+            utilities.createButton('Prev', response.prevPageToken,
+                function (event) {
+                    player.showHighScoreList(event.target.value, false);
+                }));
+    }
+};
+
+player.showHighScoreList = function (pageToken, isTestMode) {
+    isTestMode = isTestMode || false;
     document.querySelector('#highScoreListDiv').innerHTML = '';
     document.querySelector('#highScoreListDiv').style.display = 'block';
     // Create the request.
@@ -9,28 +35,5 @@ player.showHighScoreList = function(pageToken) {
             timeSpan: 'all_time',
             pageToken: pageToken,
             maxResults: '10'});
-    request.execute(
-        function(response) {
-            console.log('High score', response);
-            if (response.error) {
-                alert('Error ' + response.error.code + ': ' + response.message);
-                return;
-            }
-            var root = document.getElementById('highScoreListDiv');
-            player.createPlayerList(root, response.items, true);
-            if (response.prevPageToken) {
-                root.appendChild(
-                    utilities.createButton('Prev', response.prevPageToken,
-                        function(event) {
-                            player.showHighScoreList(event.target.value);
-                        }));
-            }
-            if (response.nextPageToken) {
-                root.appendChild(
-                    utilities.createButton('Prev', response.prevPageToken,
-                        function(event) {
-                            player.showHighScoreList(event.target.value);
-                        }));
-            }
-        });
+    request.execute(executeRequest.bind(this, isTestMode));
 };
